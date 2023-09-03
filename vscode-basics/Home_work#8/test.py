86f660c39ac85e0b5be0b3b3d357eb88a547e905
Chonna86 <alexandrchonka@gmail.com>
2023-09-03 22:40:09 +0300

    second_c

--- vscode-basics/Home_work#8/test.py
+++
@@ -1,7 +0,0 @@
-from datetime import date, datetime, timedelta
-today = date.today()
-print(today)
-target_date = date(year=2024,month=1,day=1)
-print(target_date)
-differnce = target_date - today
-print(differnce)
