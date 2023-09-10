bad991ebff868ed23c936e5d32c4914b24706200
Chonna86 <alexandrchonka@gmail.com>
2023-09-10 19:21:33 +0300

    last_commit_10_09

--- vscode-basics/HomeWork#9.py
+++ vscode-basics/HomeWork#9.py
@@ -1,5 +1,9 @@
+
+
 contacts = {}
 
+
+# First, let's create a decorator function to handle errors
 def input_error(func):
     def wrapper(*args, **kwargs):
         try:
@@ -12,24 +16,32 @@ def input_error(func):
             return "Invalid command format"
     return wrapper
 
+# Now we call the decorator function for each of the following functions on user input.
+# Next let's create a function for entering user input into our dictionary(contacts = {})
 @input_error
 def add_contact(command):
     _, name, phone = command.split()
     contacts[name] = phone
     return f"Contact {name} added with phone number {phone}"
 
+
+'''Now let's create a function to enter the data entered by the user into our dictionary, 
+if the user has changed the phone number of an existing contact in the dictionary''' 
 @input_error
 def change_phone(command):
     _, name, new_phone = command.split()
     contacts[name] = new_phone
     return f"Phone number for {name} updated to {new_phone}"
 
+
+# Now let's create a function to display the phone number of the contact whose name the user entered
 @input_error
 def get_phone(command):
     _, name = command.split()
     phone = contacts.get(name, "Contact not found")
     return f"Phone number for {name}: {phone}"
 
+# Now let's create a function to output all the data entered by the user
 @input_error
 def show_all_contacts(command):
     if not contacts:
@@ -37,6 +49,9 @@ def show_all_contacts(command):
     contact_list = "\n".join([f"{name}: {phone}" for name, phone in contacts.items()])
     return contact_list
 
+
+'''Now let's create the main function, which will do all the interaction 
+with the user and will perform all the logic of the program'''
 def main():
     print("How can I help you?")
     while True:
