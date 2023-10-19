90dca89165fdf56b76a729b07bfc6ba4fe8ee6c8
Oleksandr Chonka <alexandrchonka@gmail.com>
2023-10-19 18:52:18 +0300

    last

--- vscode-basics/fun.py
+++
@@ -1,3 +0,0 @@
-def get_fullname(first_name, last_name, middle_name='') :
-        return f"{first_name} {middle_name} {last_name}"
-print(get_fullname("Oleksandr", "Chonka",))
--- vscode-basics/list_in_list.py
+++
@@ -1,8 +0,0 @@
-def game(terra, power):
-    result = power
-    for i in terra :
-        for j in i :
-            if j <= power :
-                result += j
-    return result
-print(game([[1,2,3,4],[2,3,4,1],[1,1,1,1]],2))                   
